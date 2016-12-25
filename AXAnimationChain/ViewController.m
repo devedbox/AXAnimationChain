d073925a476b632d570d2615fb28667223033413
devedbox <devedbox@gmail.com>
2016-12-25 21:16:29 +0800

    Added target-action handler.

--- AXAnimationChain/ViewController.m
+++ AXAnimationChain/ViewController.m
@@ -59,13 +59,18 @@
 
 //    _transitionView.animationChain.basic.property(@"position").toValue([NSValue valueWithCGPoint:CGPointMake(100, 300)]).duration(0.5).nextTo(_transitionView.animationChain.basic).property(@"transform.rotation").toValue(@(M_PI_4)).easeInOut.duration(1.5).nextTo(_transitionView.animationChain.basic).property(@"bounds").toValue([NSValue valueWithCGRect:CGRectMake(0, 0, 100, 100)]).duration(2.0).nextTo(_transitionView.animationChain.basic).property(@"position").toValue([NSValue valueWithCGPoint:self.view.center]).duration(2.0).animate();
     
-    _transitionView.chainAnimator.basic.property(@"position").toValue([NSValue valueWithCGPoint:CGPointMake(100, self.view.center.y)]).easeOut.duration(0.5).combineSpring.property(@"bounds").toValue([NSValue valueWithCGRect:CGRectMake(0, 0, 100, 100)]).duration(0.5).repeatCount(10).autoreverses.combineSpring.property(@"transform.rotation").toValue(@(M_PI_4)).duration(0.5).repeatCount(3).beginTime(1.0).autoreverses.nextToBasic.property(@"position").toValue([NSValue valueWithCGPoint:self.view.center]).duration(0.5).combineSpring.property(@"bounds").toValue([NSValue valueWithCGRect:CGRectMake(0, 0, 100, 100)]).duration(0.8).nextToSpring.property(@"transform.rotation").toValue(@(M_PI_4)).duration(1.0).animate();
+    _transitionView.chainAnimator.basic.target(self).complete(@selector(complete:)).property(@"position").toValue([NSValue valueWithCGPoint:CGPointMake(100, self.view.center.y)]).easeOut.duration(0.5).combineSpring.target(self).complete(@selector(complete:)).property(@"bounds").toValue([NSValue valueWithCGRect:CGRectMake(0, 0, 100, 100)]).duration(0.5).repeatCount(10).autoreverses.combineSpring.target(self).complete(@selector(complete:)).property(@"transform.rotation").toValue(@(M_PI_4)).duration(0.5).repeatCount(3).beginTime(1.0).autoreverses.nextToBasic.property(@"position").toValue([NSValue valueWithCGPoint:self.view.center]).duration(0.5).combineSpring.property(@"bounds").toValue([NSValue valueWithCGRect:CGRectMake(0, 0, 100, 100)]).duration(0.8).nextToSpring.property(@"transform.rotation").toValue(@(M_PI_4)).duration(1.0).animate();
+    self.view.spring.backgroundColorTo([UIColor colorWithRed:1.000 green:0.988 blue:0.922 alpha:1.00]).animate();
     
 //    _transitionView.animationChain.basic.property(@"position").toValue([NSValue valueWithCGPoint:CGPointMake(100, 300)]).easeInOut.duration(0.5).repeatCount(2).repeatDuration(.0).autoreverses.animate();
     
 //    _transitionView.animationChain.spring.property(@"bounds").duration(1.5).toValue([NSValue valueWithCGRect:CGRectMake(0, 0, 100, 100)]).autoreverses.animate();
     
 //    _transitionView.animationChain.basic.property(@"position").easeInOut.byValue([NSValue valueWithCGPoint:CGPointMake(0, 300)]).duration(1.0).combineWith(_transitionView.animationChain.spring.property(@"transform.scale")).beginTime(0.5).duration(1.0).toValue(@(0.5)).animate();
+}
+
+- (void)complete:(AXChainAnimator *)sender {
+    NSLog(@"%@", sender);
 }
 
 - (IBAction)simpleHandler:(id)sender {
