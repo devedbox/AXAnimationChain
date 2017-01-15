0338467cb9e601c6d95cfb7a28e93569abf9ddd9
devedbox <862099730@qq.com>
2017-01-15 19:15:10 +0800

    Added nullbilty.

--- AXAnimationChain/Classes/AXChainAnimator+Block.h
+++ AXAnimationChain/Classes/AXChainAnimator+Block.h
@@ -24,7 +24,7 @@
 //  SOFTWARE.
 
 #import "AXChainAnimator.h"
-
+NS_ASSUME_NONNULL_BEGIN
 @interface AXChainAnimator (Block)
 /// Begin with block reachable.
 ///
@@ -318,3 +318,4 @@
 ///
 - (AXTransitionChainAnimator * (^)(dispatch_block_t completion))completeWithBlock;
 @end
+NS_ASSUME_NONNULL_END
