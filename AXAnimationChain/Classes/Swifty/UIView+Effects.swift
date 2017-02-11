dd65f5848e7948eb15cef5048c462c542b2987ab
devedbox <devedbox@gmail.com>
2017-02-11 15:22:46 +0800

    Fix...

--- AXAnimationChain/Classes/Swifty/UIView+Effects.swift
+++ AXAnimationChain/Classes/Swifty/UIView+Effects.swift
@@ -52,7 +52,7 @@ private func settlingDurationForSpring(mass: Double, stiffness: Double, damping:
 public extension UIView {
     /// Run a \`tada\` animation effect on the receiver.
     ///
-    /// - parameter completion: a completion clousure to execute when the animation finished.
+    /// - parameter completion: a completion closure to execute when the animation finished.
     ///
     public func tada(completion: @escaping () -> Void = {}) {
         chainAnimator.basic.property("transform.scale").fromValue(1.0).toValue(1.1).duration(0.15).combineBasic().property("transform.rotation").byValue(M_PI/21.0).duration(0.1).autoreverses().repeatCount(2).combineBasic().beginTime(0.1).property("transform.rotation").byValue(-M_PI/18.0).duration(0.1).autoreverses().repeatCount(2).nextToBasic().property("transform.scale").toValue(1.0).duration(0.15).start(completion: completion)
@@ -61,7 +61,7 @@ public extension UIView {
     ///
     /// - Parameters:
     ///   - from: the from direction of the bonuce animation showing up.
-    ///   - completion: a completion clousure to execute when the animation finished.
+    ///   - completion: a completion closure to execute when the animation finished.
     ///
     public func bonuce(from direction: AXAnimationEffectsDirection = .top, completion: @escaping () -> Void = {}) {
         switch direction {
@@ -77,14 +77,14 @@ public extension UIView {
     }
     /// Run a \`pulse\` animation effect on the receiver.
     ///
-    /// - Parameter completion: a completion clousure to execute when the animation finished.
+    /// - Parameter completion: a completion closure to execute when the animation finished.
     ///
     public func pulse(completion: @escaping () -> Void = {}) {
         chainAnimator.basic.property("transform.scale").byValue(0.1).duration(0.5).linear().autoreverses().start(completion: completion)
     }
     /// Run a \`shake\` animation effect on the receiver.
     ///
-    /// - Parameter completion: a completion clousure to execute when the animation finished.
+    /// - Parameter completion: a completion closure to execute when the animation finished.
     ///
     public func shake(completion: @escaping () -> Void = {}) {
         let position = layer.position
@@ -92,7 +92,7 @@ public extension UIView {
     }
     /// Run a \`swing\` animation effect on the receiver.
     ///
-    /// - Parameter completion: a completion clousure to execute when the animation finished.
+    /// - Parameter completion: a completion closure to execute when the animation finished.
     ///
     public func swing(completion: @escaping () -> Void = {}) {
         chainAnimator.basic.property("transform.rotation").byValue(M_PI/21.0).duration(0.1).autoreverses().repeatCount(2).combineBasic().beginTime(0.1).property("transform.rotation").byValue(-M_PI/18.0).duration(0.1).autoreverses().repeatCount(2).nextToBasic().property("transform.scale").toValue(1.0).duration(0.15).start(completion: completion)
@@ -107,21 +107,21 @@ public extension UIView {
     }
     /// Run a \`expand\` animation effect on the receiver.
     ///
-    /// - Parameter completion: a completion clousure to execute when the animation finished.
+    /// - Parameter completion: a completion closure to execute when the animation finished.
     ///
     public func expand(completion:@escaping () -> Void = {}) {
         chainAnimator.basic.property("transform.scale").fromValue(0.0).toValue(1.0).duration(0.5).start(completion: completion)
     }
     /// Run a \`compress\` animation effect on the receiver.
     ///
-    /// - Parameter completion: a completion clousure to execute when the animation finished.
+    /// - Parameter completion: a completion closure to execute when the animation finished.
     ///
     public func compress(completion: @escaping () -> Void = {}) {
         chainAnimator.basic.property("transform.scale").fromValue(1.0).toValue(0.0).duration(0.5).start(completion: completion)
     }
     /// Run a \`hinge\` animation effect on the receiver.
     ///
-    /// - Parameter completion: a completion clousure to execute when the animation finished.
+    /// - Parameter completion: a completion closure to execute when the animation finished.
     ///
     public func hinge(completion: @escaping () -> Void = {}) {
         layer.anchorToLeftTop()
@@ -129,7 +129,7 @@ public extension UIView {
     }
     /// Run a \`drop\` animation effect on the receiver.
     ///
-    /// - Parameter completion: a completion clousure to execute when the animation finished.
+    /// - Parameter completion: a completion closure to execute when the animation finished.
     ///
     public func drop(completion: @escaping () -> Void = {}) {
         chainAnimator.basic.anchorToRightTop().property("transform.rotation").toValue(-M_PI/18*2).duration(1.0).combineBasic().property("position.y").fromValue(layer.position.y).toValue(UIScreen.main.bounds.height+layer.position.y).duration(0.5).easeInCubic().start(completion: completion)
