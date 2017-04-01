6bf4a1dd29234063770adf060f969c6ee3ff8134
devedbox <devedbox@gmail.com>
2017-04-01 16:49:21 +0800

    Added decay animation view controller.

---
+++ AXAnimationChain/DecayAnimationViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DecayAnimationViewController.swift
+//  AXAnimationChain
+//
+//  Created by devedbox on 2017/4/1.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import UIKit
+import AXAnimationChainSwift
+
+class DecayAnimationViewController: UIViewController {
+
+    @IBOutlet weak var animatedView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
