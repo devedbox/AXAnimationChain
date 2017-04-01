1d26f4b466edb421c32be30f327bc26e763f1185
devedbox <862099730@qq.com>
2017-04-01 23:20:16 +0800

    Formatted codes.

--- AXAnimationChain/Classes/CoreAnimation/AXDecayAnimation.m
+++ AXAnimationChain/Classes/CoreAnimation/AXDecayAnimation.m
@@ -28,10 +28,10 @@
 #import "AXDecayAnimation.h"
 #import "CAMediaTimingFunction+Extends.h"
 
-extern NSArray * UIColorValuesWithComponents(NSArray *redValues, NSArray *greenValues, NSArray *blueValues, NSArray *alphaValues);
-extern NSArray * CGRectValuesWithComponents(NSArray *xValues, NSArray *yValues, NSArray *widths, NSArray *heights);
 extern NSArray * CGPointValuesWithComponents(NSArray *xValues, NSArray *yValues);
 extern NSArray * CGSizeValuesWithComponents(NSArray *widthValues, NSArray *heightValues);
+extern NSArray * CGRectValuesWithComponents(NSArray *xValues, NSArray *yValues, NSArray *widths, NSArray *heights);
+extern NSArray * UIColorValuesWithComponents(NSArray *redValues, NSArray *greenValues, NSArray *blueValues, NSArray *alphaValues);
 extern NSArray * CATransform3DValuesWithComponents(NSArray *m11, NSArray *m12, NSArray *m13, NSArray *m14, NSArray *m21, NSArray *m22, NSArray *m23, NSArray *m24, NSArray *m31, NSArray *m32, NSArray *m33, NSArray *m34, NSArray *m41, NSArray *m42, NSArray *m43, NSArray *m44);
 
 static NSArray * _AXDecayNSNumberValuesCaculationWithBeginValue(CGFloat beginNumber, NSTimeInterval duration, CAMediaTimingFunction *timing, CAKeyframeValuesFunction function) {
@@ -68,26 +68,58 @@ NSArray * _AXDecayKeyframeValuesWithBeginFrame(id fromValue, NSTimeInterval dura
     beginValue = fromValue;
     
     if ([beginValue isKindOfClass:[NSNumber class]]) {
-        return _AXDecayNSNumberValuesCaculationWithBeginValue([beginValue floatValue], duration, timing, function);
+        return
+        _AXDecayNSNumberValuesCaculationWithBeginValue([beginValue floatValue], duration, timing, function);
     } else if ([beginValue isKindOfClass:[UIColor class]]) {
         const CGFloat *fromComponents = CGColorGetComponents(((UIColor*)beginValue).CGColor);
-        return UIColorValuesWithComponents(_AXDecayNSNumberValuesCaculationWithBeginValue(fromComponents[0], duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromComponents[1], duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromComponents[2], duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromComponents[3], duration, timing, function));
+        return UIColorValuesWithComponents(
+                                           _AXDecayNSNumberValuesCaculationWithBeginValue(fromComponents[0], duration, timing, function),
+                                           _AXDecayNSNumberValuesCaculationWithBeginValue(fromComponents[1], duration, timing, function),
+                                           _AXDecayNSNumberValuesCaculationWithBeginValue(fromComponents[2], duration, timing, function),
+                                           _AXDecayNSNumberValuesCaculationWithBeginValue(fromComponents[3], duration, timing, function)
+                                           );
     } else if ([beginValue isKindOfClass:[NSValue class]]) {
         NSString *valueType = [NSString stringWithCString:[beginValue objCType] encoding:NSStringEncodingConversionAllowLossy];
         if ([valueType rangeOfString:@"CGRect"].location == 1) {
             CGRect fromRect = [beginValue CGRectValue];
-            return CGRectValuesWithComponents(_AXDecayNSNumberValuesCaculationWithBeginValue(fromRect.origin.x, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromRect.origin.y, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromRect.size.width, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromRect.size.height, duration, timing, function));
+            return CGRectValuesWithComponents(_AXDecayNSNumberValuesCaculationWithBeginValue(fromRect.origin.x, duration, timing, function),
+                                              _AXDecayNSNumberValuesCaculationWithBeginValue(fromRect.origin.y, duration, timing, function),
+                                              _AXDecayNSNumberValuesCaculationWithBeginValue(fromRect.size.width, duration, timing, function),
+                                              _AXDecayNSNumberValuesCaculationWithBeginValue(fromRect.size.height, duration, timing, function));
             
         } else if ([valueType rangeOfString:@"CGPoint"].location == 1) {
             CGPoint fromPoint = [beginValue CGPointValue];
-            return CGPointValuesWithComponents(_AXDecayNSNumberValuesCaculationWithBeginValue(fromPoint.x, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromPoint.y, duration, timing, function));
+            return CGPointValuesWithComponents(
+                                               _AXDecayNSNumberValuesCaculationWithBeginValue(fromPoint.x, duration, timing, function),
+                                               _AXDecayNSNumberValuesCaculationWithBeginValue(fromPoint.y, duration, timing, function)
+                                               );
             
         } else if ([valueType rangeOfString:@"CATransform3D"].location == 1) {
             CATransform3D fromTransform = [beginValue CATransform3DValue];
-            return CATransform3DValuesWithComponents(_AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m11, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m12, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m13, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m14, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m21, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m22, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m23, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m24, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m31, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m32, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m33, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m34, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m41, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m42, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m43, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m44, duration, timing, function));
+            return CATransform3DValuesWithComponents(
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m11, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m12, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m13, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m14, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m21, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m22, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m23, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m24, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m31, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m32, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m33, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m34, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m41, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m42, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m43, duration, timing, function),
+                                                     _AXDecayNSNumberValuesCaculationWithBeginValue(fromTransform.m44, duration, timing, function)
+                                                     );
         } else if ([valueType rangeOfString:@"CGSize"].location == 1) {
             CGSize fromSize = [beginValue CGSizeValue];
-            return CGSizeValuesWithComponents(_AXDecayNSNumberValuesCaculationWithBeginValue(fromSize.width, duration, timing, function), _AXDecayNSNumberValuesCaculationWithBeginValue(fromSize.height, duration, timing, function));
+            return CGSizeValuesWithComponents(
+                                              _AXDecayNSNumberValuesCaculationWithBeginValue(fromSize.width, duration, timing, function),
+                                              _AXDecayNSNumberValuesCaculationWithBeginValue(fromSize.height, duration, timing, function)
+                                              );
         }
     }
     return nil;
