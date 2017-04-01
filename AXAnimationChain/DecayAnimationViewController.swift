a035919a520675fefc5fb7fefc3c2bf14f4ad38e
devedbox <862099730@qq.com>
2017-04-01 23:07:43 +0800

    Added sample code.

--- AXAnimationChain/DecayAnimationViewController.swift
+++ AXAnimationChain/DecayAnimationViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AXAnimationChainSwift
 
-class DecayAnimationViewController: UIViewController {
+class DecayAnimationViewController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var animatedView: UIView!
     
@@ -17,13 +17,72 @@ class DecayAnimationViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // Add pan ges ture to the animated view.
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+        pan.delegate = self
+        animatedView.addGestureRecognizer(pan)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: Actions.
+    @objc
+    private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        animatedView.layer.removeAllAnimations()
+        
+        var point = gesture.location(in: view)
+        
+        switch gesture.state {
+        case .began:
+            point = gesture.location(in: view)
+        case .possible: break
+        case .changed:
+            /*
+            point.x = max(animatedView.frame.width/2+64, point.x)
+            point.y = max(animatedView.frame.height/2+64, point.y)
+            point.x = min(view.frame.width-animatedView.frame.width/2, point.x)
+            point.y = min(view.frame.height-animatedView.frame.height/2, point.y)
+             */
+            animatedView.center = point
+        case .ended:
+            point = gesture.location(in: view)
+            print("view's center:\(String(describing: animatedView.center)), layer's position:\(String(describing: animatedView.layer.position))")
+            
+            let ve = gesture.velocity(in: animatedView)
+            let decayx = AXDecayAnimation(keyPath: "position.x")
+            decayx.fromValue = point.x
+            decayx.velocity = ve.x
+            decayx.isRemovedOnCompletion=false
+            decayx.fillMode=kCAFillModeForwards
+            // decayx.isAdditive = true
+            let decayy = AXDecayAnimation(keyPath: "position.y")
+            decayy.fromValue = point.y
+            decayy.velocity = ve.y
+            decayy.isRemovedOnCompletion=false
+            decayy.fillMode=kCAFillModeForwards
+            // decayy.isAdditive = true
+            CATransaction.setCompletionBlock({[weak self] () -> Void in
+                print("view's center:\(String(describing: self?.animatedView.center)), layer's position:\(String(describing: self?.animatedView.layer.position))")
+                self?.animatedView.layer.position = CGPoint(x: decayx.values?.last as! Double, y: decayy.values?.last as! Double)
+                self?.animatedView.layer.removeAllAnimations()
+                print("view's center:\(String(describing: self?.animatedView.center)), layer's position:\(String(describing: self?.animatedView.layer.position))")
+            })
+            CATransaction.begin()
+            CATransaction.setDisableActions(false)
+            animatedView.layer.add(decayx, forKey: "position.x")
+            animatedView.layer.add(decayy, forKey: "position.y")
+            CATransaction.commit()
+            
+        case .cancelled: break
+        case .failed: break
+        // default: break
+        }
+    }
+    
+    // MARK: UIGestureRecognizerDelegate.
 
     /*
     // MARK: - Navigation
