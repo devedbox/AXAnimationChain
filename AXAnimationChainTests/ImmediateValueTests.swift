7fa06d69b2dd05dd777dfa7c835a741ba9319bf8
devedbox <devedbox@gmail.com>
2017-05-23 10:12:09 +0800

    Added ImmediateValueTests.swift file.

---
+++ AXAnimationChainTests/ImmediateValueTests.swift
@@ -0,0 +1,35 @@
+//
+//  ImmediateValueTests.swift
+//  AXAnimationChain
+//
+//  Created by devedbox on 2017/5/23.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import XCTest
+
+class ImmediateValueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
